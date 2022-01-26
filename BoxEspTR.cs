30fbb4a61ff2c1689a5fc7f4736a7c9bdeb3715f
LunaUI <59039458+LunaValerie@users.noreply.github.com>
2022-01-26 19:12:10 +0300

    Point Blank TR ESP BoX
    Free? Really Yes Free

---
+++ BoxEspTR.cs
@@ -0,0 +1,12 @@
+   #region MEM
+        // Luna Valerie / Abone Olmayı Unutmyaın Lütfennnn
+        string VIEWMATRIX = "PointBlank.exe+00A23FE8,458,2C0"; // Matrix
+        string LOCALPLAYER = "PointBlank.exe+00F41618"; // LocalPlayer
+        int ENTITYBASE = 0x00F38730; // EntityBase // Ben Luna
+        string HP = ",0x36C"; //  Health Sart ( yanlıs olursa esp çizilmez)
+        string XYZ = ",0x260"; //  X,Y,Z Yanlış Olsada ESP Çizlir
+        string TEAM = ",0x9c"; // SARK Değil ESP Çizilir
+        string DORMANT = ",0x17C"; // Dormant ( Sart Değil ESP Çizilir )
+        string GAME = "PointBlank.exe+"; // Game ( Çektiginiz Offsets neyse o)
+
+        #endregion
